bcda76e4f0f7fe0d9290edd8f5a4b95de0f002e0
Benjamin McFarland <benrmcfarland@gmail.com>
2020-10-23 14:23:14 -0700

    Add js to only have one question show at a time

--- js/scripts.js
+++ js/scripts.js
@@ -15,9 +15,101 @@ $(document).ready(function() {
     const backColor = $("#color").val();
     document.body.style.background = backColor;
     $(".color-select").fadeOut();
-    $(".question").fadeIn();
+    $("#question-one").fadeIn();
     $("img").fadeIn();
   });
+
+  $("#question1-next").click(function(event) {
+    event.preventDefault();
+    $("#question-one").hide();
+    $("#question-two").show();
+  });
+  $("#question2-next").click(function(event) {
+    event.preventDefault();
+    $("#question-two").hide();
+    $("#question-three").show();
+  });
+  $("#question2-back").click(function(event) {
+    event.preventDefault();
+    $("#question-two").hide();
+    $("#question-one").show();
+  });
+  $("#question3-next").click(function(event) {
+    event.preventDefault();
+    $("#question-three").hide();
+    $("#question-four").show();
+  });
+  $("#question3-back").click(function(event) {
+    event.preventDefault();
+    $("#question-three").hide();
+    $("#question-two").show();
+  });
+  $("#question4-next").click(function(event) {
+    event.preventDefault();
+    $("#question-four").hide();
+    $("#question-five").show();
+  });
+  $("#question4-back").click(function(event) {
+    event.preventDefault();
+    $("#question-four").hide();
+    $("#question-three").show();
+  });
+  $("#question5-next").click(function(event) {
+    event.preventDefault();
+    $("#question-five").hide();
+    $("#question-six").show();
+  });
+  $("#question5-back").click(function(event) {
+    event.preventDefault();
+    $("#question-five").hide();
+    $("#question-four").show();
+  });
+  $("#question6-next").click(function(event) {
+    event.preventDefault();
+    $("#question-six").hide();
+    $("#question-seven").show();
+  });
+  $("#question6-back").click(function(event) {
+    event.preventDefault();
+    $("#question-six").hide();
+    $("#question-five").show();
+  });
+  $("#question7-next").click(function(event) {
+    event.preventDefault();
+    $("#question-seven").hide();
+    $("#question-eight").show();
+  });
+  $("#question7-back").click(function(event) {
+    event.preventDefault();
+    $("#question-seven").hide();
+    $("#question-six").show();
+  });
+  $("#question8-next").click(function(event) {
+    event.preventDefault();
+    $("#question-eight").hide();
+    $("#question-nine").show();
+  });
+  $("#question8-back").click(function(event) {
+    event.preventDefault();
+    $("#question-eight").hide();
+    $("#question-seven").show();
+  });
+  $("#question9-next").click(function(event) {
+    event.preventDefault();
+    $("#question-nine").hide();
+    $("#question-ten").show();
+  });
+  $("#question9-back").click(function(event) {
+    event.preventDefault();
+    $("#question-nine").hide();
+    $("#question-eight").show();
+  });
+  $("#question10-back").click(function(event) {
+    event.preventDefault();
+    $("#question-ten").hide();
+    $("#question-nine").show();
+  });
+
   $("form#survey").submit(function(event) {
     event.preventDefault();
     const answer1 = parseInt($("input:radio[name=experience]:checked").val());
